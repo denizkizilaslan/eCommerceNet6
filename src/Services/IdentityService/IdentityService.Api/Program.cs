efb78c38f882663d864cc7c79e181a0968ecae0a
deniz kizilaslan <kizilaslandeniz@gmail.com>
2022-10-03 21:16:05 +0300

    Identity Service added - JWT Token

---
+++ src/Services/IdentityService/IdentityService.Api/Program.cs
@@ -0,0 +1,29 @@
+using IdentityService.Api.Application.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddScoped<IIdentityService, IdentityService.Api.Application.Services.IdentityService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
